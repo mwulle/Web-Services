43d2649858f8a462507fd581f10d66b825e544de
Melvin Wulle <hey@mwulle.com>
2023-03-10 11:07:47 +0100

    Implement gRPC Client

--- CalculatorClient/Program.cs
+++ CalculatorClient/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace CalculatorClient
+﻿using Grpc.Net.Client;
+
+namespace CalculatorClient
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Disable TLS for MacOS
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
+            using var channel = GrpcChannel.ForAddress("http://localhost:5019");
+            var client = new Calculator.CalculatorClient(channel);
+
+            var addRequest = new AddRequest()
+            {
+                Number1 = 1,
+                Number2 = 3
+            };
+
+            var result = client.Add(addRequest);
+            
+            Console.WriteLine(result.Sum);
         }
     }
 }
