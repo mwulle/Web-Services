c40402275cbc5a547a4d0211c4c89b4dd893bb9b
Melvin Wulle <hey@mwulle.com>
2023-03-28 10:23:03 +0200

    Add Service Bus Consumer and Service

---
+++ CalculatorSBConsumer/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Azure.Messaging.ServiceBus;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace CalculatorSBConsumer;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        using IHost host = Host.CreateDefaultBuilder(args).UseEnvironment("Development").Build();
+        IConfiguration config = host.Services.GetRequiredService<IConfiguration>();
+        var connection = config.GetValue<string>("connection");
+        
+        await using var client = new ServiceBusClient(connection);
+        await using var sender = client.CreateSender("queue-1");
+
+        var numbers = new Numbers { Number1 = 1, Number2 = 2 };
+        var json = System.Text.Json.JsonSerializer.Serialize(numbers);
+        var body = System.Text.Encoding.UTF8.GetBytes(json);
+
+        var message = new ServiceBusMessage(body);
+        message.CorrelationId = Guid.NewGuid().ToString();
+        
+        await sender.SendMessageAsync(message);
+        Console.WriteLine("Message send");
+    }
+}
+
+public class Numbers
+{
+    public double Number1 { get; set; }
+    public double Number2 { get; set; }
+}
---
+++ CalculatorSBService/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+using Azure.Messaging.ServiceBus;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace CalculatorSBService;
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        using IHost host = Host.CreateDefaultBuilder(args).UseEnvironment("Development").Build();
+        IConfiguration config = host.Services.GetRequiredService<IConfiguration>();
+        var connection = config.GetValue<string>("connection");
+        
+        await using var client = new ServiceBusClient(connection);
+        var options = new ServiceBusReceiverOptions()
+        {
+            ReceiveMode = ServiceBusReceiveMode.PeekLock
+        };
+        var receiver = client.CreateReceiver("queue-1");
+
+        while (true)
+        {
+            var message = await receiver.ReceiveMessageAsync();
+            if (message == null)
+            {
+                Console.WriteLine("No available messages");
+                await Task.Delay(100);
+                continue;
+            }
+            
+            Console.WriteLine("Received: " + message.MessageId);
+
+            var body = message.Body;
+            if (body != null)
+            {
+                var json = Encoding.UTF8.GetString(body);
+                var numbers = System.Text.Json.JsonSerializer.Deserialize<Numbers>(json);
+            
+                await receiver.CompleteMessageAsync(message);
+            }
+            else
+            {
+                await receiver.DeadLetterMessageAsync(message);
+            }
+        }
+    }
+}
+
+public class Numbers
+{
+    public double Number1 { get; set; }
+    public double Number2 { get; set; }
+}
